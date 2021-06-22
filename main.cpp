01b093beba123137e303960eed84a041710c2f6f
Heider Sanchez <heider.sanchez.pe@gmail.com>
2021-06-22 12:43:49 -0500

    update main

--- main.cpp
+++ main.cpp
@@ -1,98 +1,99 @@
 #include <iostream>
 #include "hashsparsematrix.h"
 #include "listsparsematrix.h"
+#include "tester.h"
+
+using namespace std;
 
 template<typename T>
 void generateRandom(SparseMatrix<T> *matrix, float factor);
 void comparePerformance();
-void  testHashSparse(int option);
-void  testListSparse(int option);
+void  testHashSparse();
+void  testListSparse();
 
 int main(int argc, char const *argv[])
 {
-    int  option1, option2;
-    option1 = atoi(argv[1]);     
-    option2 = atoi(argv[2]);      
-    if(option1 == 1)
-       testHashSparse(option2);
-    else
-       testListSparse(option2);    
+    testHashSparse();
+    testListSparse();    
     return 0;
 }
 
-void  testHashSparse(int option){
-    HashSparseMatrix<int> *matrix1, *matrix2, matrix3;
-    unsigned rows, cols, count, posx, posy;
-    int value;
-    cin>>rows>>cols>>count;
-    matrix1 = new HashSparseMatrix<int>(rows, cols);
-    matrix2 = new HashSparseMatrix<int>(rows, cols);
-    for (int i = 0; i < count; i++)
-    {
-        cin>>posx>>posy>>value;
-        matrix1->set(posx, posy, value);
-    }
-    for (int i = 0; i < count; i++)
-    {
-        cin>>posx>>posy>>value;
-        matrix2->set(posx, posy, value);
-    }
-    switch (option)
-    {
-    case 1:
-        matrix3 = (*matrix1) + (*matrix2);
-        break;
-    case 2:
-        matrix3 = (*matrix1) - (*matrix2);
-        break;
-    case 3:
-        matrix3 = (*matrix1) * (*matrix2);
-        break;
-    case 4:
-        matrix3 = (*matrix1) * 2 + (*matrix2) * 3;
-        break;
-    default:
-        break;
-    }
+void  testHashSparse(){
+    HashSparseMatrix<int> matrix1, matrix2, matrix3;
+    unsigned rows = 5, cols = 5, count = 3;
+    matrix1 = HashSparseMatrix<int>(rows, cols);
+    matrix1.set(0, 0, 2);
+    matrix1.set(2, 1, 3);
+    matrix1.set(2, 3, 1);
+    matrix1.set(3, 0, 2);
+    matrix1.set(4, 4, 2);
+    cout<<"Matrix 1:\n";
+    matrix1.display();
+
+    matrix2 = HashSparseMatrix<int>(rows, cols);
+    matrix2.set(0, 1, 2);
+    matrix2.set(2, 3, 3);
+    matrix2.set(3, 0, 1);
+    matrix2.set(3, 4, 2);
+    matrix2.set(4, 4, 1);
+    cout<<"Matrix 2:\n";
+    matrix2.display();
+    
+    matrix3 = matrix1 + matrix2;        
+    cout<<"\nSuma:\n";
+    matrix3.display();
+
+    matrix3 = matrix1 - matrix2;    
+    cout<<"\nResta:\n";
+    matrix3.display();
+
+    matrix3 = matrix1 * matrix2;    
+    cout<<"\nMultiplicacion Matricial:\n";
+    matrix3.display();
+
+    matrix3 = (matrix1 * 2) + (matrix2 * 3);       
+    cout<<"\Multiplicacion Escalar:\n";
     matrix3.display();
 }
 
-void  testListSparse(int option){
-    ListSparseMatrix<int> *matrix1, *matrix2, matrix3;
-    unsigned rows, cols, count, posx, posy;
-    int value;
-    cin>>rows>>cols>>count;
-    matrix1 = new ListSparseMatrix<int>(rows, cols);
-    matrix2 = new ListSparseMatrix<int>(rows, cols);
-    for (int i = 0; i < count; i++)
-    {
-        cin>>posx>>posy>>value;
-        matrix1->set(posx, posy, value);
-    }
-    for (int i = 0; i < count; i++)
-    {
-        cin>>posx>>posy>>value;
-        matrix2->set(posx, posy, value);
-    }
-    switch (option)
-    {
-    case 1:
-        matrix3 = (*matrix1) + (*matrix2);
-        break;
-    case 2:
-        matrix3 = (*matrix1) - (*matrix2);
-        break;
-    case 3:
-        matrix3 = (*matrix1) * (*matrix2);
-        break;
-    case 4:
-        matrix3 = (*matrix1) * 2 + (*matrix2) * 3;
-        break;
-    default:
-        break;
-    }
+void  testListSparse(){
+    ListSparseMatrix<int> matrix1, matrix2, matrix3;
+    unsigned rows = 5, cols = 5, count = 3;
+    matrix1 = ListSparseMatrix<int>(rows, cols);
+    matrix1.set(0, 0, 2);
+    matrix1.set(2, 1, 3);
+    matrix1.set(2, 3, 1);
+    matrix1.set(3, 0, 2);
+    matrix1.set(4, 4, 2);
+    cout<<"Matrix 1:\n";
+    matrix1.display();
+
+    matrix2 = ListSparseMatrix<int>(rows, cols);
+    matrix2.set(0, 1, 2);
+    matrix2.set(2, 3, 3);
+    matrix2.set(3, 0, 1);
+    matrix2.set(3, 4, 2);
+    matrix2.set(4, 4, 1);
+    cout<<"Matrix 2:\n";
+    matrix2.display();
+    
+    matrix3 = matrix1 + matrix2;        
+    cout<<"\nSuma:\n";
+    matrix3.display();
+
+    matrix3 = matrix1 - matrix2;    
+    cout<<"\nResta:\n";
+    matrix3.display();
+
+    matrix3 = matrix1 * matrix2;    
+    cout<<"\nMultiplicacion Matricial:\n";
+    matrix3.display();
+
+    matrix3 = (matrix1 * 2) + (matrix2 * 3);       
+    cout<<"\Multiplicacion Escalar:\n";
     matrix3.display();
 }
+
 
 template<typename T>
 void generateRandom(SparseMatrix<T> *matrix, float factor){
