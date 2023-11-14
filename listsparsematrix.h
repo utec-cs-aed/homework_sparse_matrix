383d64fe70d37c0a1b4b75e7a321f23f93d24ec8
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2023-11-14 10:54:23 -0500

    Update listsparsematrix.h

--- listsparsematrix.h
+++ listsparsematrix.h
@@ -8,29 +8,29 @@ template <typename T>
 struct Node {
     public:
         T data;
-        int posX;
-        int posY;
-        Node<T>* next;//row
-        Node<T>* down;//column
+        int pos_row;
+        int pos_col;
+        Node<T>* next_row;
+        Node<T>* next_col;
 };
 
 template <class T>
 class ListSparseMatrix : public SparseMatrix<T> {
     private:
-        vector<Node<T>*> X;
-        vector<Node<T>*> Y;                
+        Node<T>** rows;
+        Node<T>** cols;                
 
     public:
         ListSparseMatrix();
         ListSparseMatrix(unsigned rows, unsigned columns);
         ~ListSparseMatrix();
 
-        bool set(unsigned posX, unsigned posY, T data);
+        bool set(unsigned i, unsigned j, T data);
 
-        T get(unsigned posX, unsigned posY);//O(1) + O(m)
-        bool remove(unsigned posX, unsigned posY);
+        T get(unsigned i, unsigned j);//O(1) + O(m)
+        bool remove(unsigned i, unsigned j);
 
-        T operator()(unsigned posX, unsigned posY);   
+        T operator()(unsigned i, unsigned j);   
         ListSparseMatrix<T>& operator * (T scalar);   
         ListSparseMatrix<T>& operator + (const ListSparseMatrix<T> &other);  
         ListSparseMatrix<T>& operator - (const ListSparseMatrix<T> &other);
