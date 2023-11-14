380960e599fbe5310d79dc0f011d8d08ddf05289
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2023-11-14 10:55:28 -0500

    Update SparseMatrix.h

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -8,9 +8,9 @@ class SparseMatrix {
         unsigned columns;
 
     public:        
-        virtual bool set(unsigned posX, unsigned posY, T data) = 0;
-        virtual T get(unsigned posX, unsigned posY) = 0;
-        virtual bool remove(unsigned posX, unsigned posY) = 0;
+        virtual bool set(unsigned i, unsigned j, T data) = 0;
+        virtual T get(unsigned i, unsigned j) = 0;
+        virtual bool remove(unsigned i, unsigned j) = 0;
         virtual void display() = 0; 
 };
 
